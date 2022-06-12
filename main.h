15e2fdc4463cd3cb4324c8b569ea9535dc7ff40c
Mohamed Magdy <magdim200@gmail.com>
2022-06-12 21:37:59 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,8 @@
+void toString (int tim, char text []); 
+void GPIO_init(void);
+void printchar(char);
+void print(char*);
+void TASK1(void *);
+void TASK2(void*);
+void TASK3(void*);
+void TASK4(void*);
